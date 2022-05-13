253c3f14cbd650b0faef0c58b07834cc2ff152cc
aparnamohan1234 <mohanaparna465@gmail.com>
2022-05-13 15:44:46 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+           <div className="row g-3">
+                  <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+            <label for="" className="form-label">username</label>
+            <input placeholder='Enter the name' type="text" className="form-control"/>
+                  </div>
+                  <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+               <label for="" className="form-label">password</label>
+               <input placeholder='Enter the password' type="" className="form-control"/>
+            </div>
+            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <button className="btn btn-success">login</button>
+            
+            </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+  
+
   );
 }
 
